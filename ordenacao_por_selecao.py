9aefbcd74300f905d5ba793f5ceba2964e30312b
Fernando Santos <nandosantos.ia@gmail.com>
2024-03-20 06:54:44 -0300

    ordenacao por selecao 1

---
+++ ordenacao_por_selecao.py
@@ -0,0 +1,11 @@
+"""
+1. Como armazenamos itens na memória do computador:
+Podemos fazer a comparação com uma gaveta, quando queremos armazenar algo o computador sede um espaço reservado e guarda apenas aquele item ali
+
+2. Listas, Listas Encadeada e Arrays:
+Vamos entende a diferença desse 3 tipos. Para montar uma lista precisamos armazenar o elemento um ao lado do outro na memória, então as "gavetas" precisam estar
+enfileiradas, porém, se eu quiser fazer um array de 10 elemento e não tiver 10 slots um em seguida do outro liberado? Meu computador vai ter que achar um espaço assim 
+para realocar minha lista.
+
+Terminar de escrever sobre lista encadeada e arrays...
+"""
